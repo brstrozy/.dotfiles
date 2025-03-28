06653ed16bf6db7e654bb010c56bb4f98dd0c625
brstrozy <benjaminstrozyk01@gmail.com>
2025-03-28 03:31:22 +0000

    added set, remap, and color theme configuration

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,48 +1,43 @@
-
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
---open file vim file explorer
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- nvim file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Move highlighted lines up and down
+-- moves selected lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
+-- scrolls half page, keeps cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- after /pattern, keep cursor centered & open folds automatically
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever - paste over without replacing content in buffer
+-- paste over selection without replacing clipboard
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- delete text without saving to clipboard
+vim.keymap.set({"n","v"}, "<leader>d", [["_d]])
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
+-- disables Q command for entering Ex mode, rarely needed
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- lsp auto formatting
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- starts gloabl search and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set("n", "<leader><leader>", function()
---    vim.cmd("so")
--- end)
-
--- Window remaps
+-- window management
 vim.keymap.set("n", "<leader>ww", ":vsp<CR>")
 vim.keymap.set("n", "<leader>wv", ":sp<CR>")
 
@@ -51,7 +46,8 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
--- Sourcing lua files to relfect changes without reloading all of nvim
+-- sourcing files quickly
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+
--- old_lua/set.lua
+++ old_lua/set.lua
@@ -1,6 +1,6 @@
-
-vim.opt.termguicolors = true
-vim.cmd.colorscheme "catppuccin"
+-- vim.opt.guicursor = ""
+vim.opt.mouse = ""
+-- vim.opt.clipboard="unnamedplus"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -9,9 +9,11 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -20,12 +22,17 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.colorcolumn = "80"
+-- vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
 vim.opt.updatetime = 50
 
+-- vim.opt.colorcolumn = "80"
+
+-- Enables system clipboard over ssh
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
@@ -37,4 +44,3 @@ vim.g.clipboard = {
     ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
   },
 }
-
