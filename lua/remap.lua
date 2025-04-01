c48d06ca9317ce32d8377ea337842928ae7f2287
brstrozy <benjaminstrozyk01@gmail.com>
2025-04-01 22:15:56 +0000

    Revert "added set, remap, and color theme configuration"
    This reverts commit 06653ed16bf6db7e654bb010c56bb4f98dd0c625.

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,43 +1,48 @@
+
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
--- nvim file explorer
+--open file vim file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- moves selected lines up and down
+-- Move highlighted lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- scrolls half page, keeps cursor centered
+vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- after /pattern, keep cursor centered & open folds automatically
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- paste over selection without replacing clipboard
+-- greatest remap ever - paste over without replacing content in buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- delete text without saving to clipboard
-vim.keymap.set({"n","v"}, "<leader>d", [["_d]])
-
--- yank to system clipboard
-vim.keymap.set("n", "<leader>y", [["+y]])
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- disables Q command for entering Ex mode, rarely needed
-vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- lsp auto formatting
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- starts gloabl search and replace
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- window management
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- vim.keymap.set("n", "<leader><leader>", function()
+--    vim.cmd("so")
+-- end)
+
+-- Window remaps
 vim.keymap.set("n", "<leader>ww", ":vsp<CR>")
 vim.keymap.set("n", "<leader>wv", ":sp<CR>")
 
@@ -46,8 +51,7 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
--- sourcing files quickly
+-- Sourcing lua files to relfect changes without reloading all of nvim
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
-
