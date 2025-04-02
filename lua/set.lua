a5a78acdd06cac3504c9f68ebe667abc56c14076
brstrozy <benjaminstrozyk01@gmail.com>
2025-04-02 03:26:23 +0000

    feat: updated set and remap config

--- lua/set.lua
+++ lua/set.lua
@@ -1,7 +1,4 @@
--- vim.opt.guicursor = ""
-vim.opt.mouse = ""
--- vim.opt.clipboard="unnamedplus"
-
+vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -9,11 +6,9 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -22,15 +17,10 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
--- vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
+--vim.opt.colorcolumn = "80"
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
--- vim.opt.colorcolumn = "80"
 
 -- Enables system clipboard over ssh
 vim.g.clipboard = {
