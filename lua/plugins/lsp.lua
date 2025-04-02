f3d2aeaf01226383d1a0d9790da3fe3c3fee3a0e
brstrozy <benjaminstrozyk01@gmail.com>
2025-04-02 01:25:01 +0000

    feat: added lazydev for lua_ls lsp setup, replaced nvim-cmp with blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,21 +15,26 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+              {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        -- Load luvit types when the `vim.uv` word is found
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+            'saghen/blink.cmp'
+        },
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            local lspconfig = require('lspconfig')
 
-            local lspconfig = require("lspconfig")
-
-            -- LSP SERVER SETUPS
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {'vim'}
-                        }
-                    }
-                }
             })
 
             lspconfig.bashls.setup({
