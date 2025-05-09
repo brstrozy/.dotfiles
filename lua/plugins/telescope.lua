42e2a0c588059ca3644cf0d03b7fa3d5e3357ea5
brstrozy <benjaminstrozyk01@gmail.com>
2025-05-09 04:33:16 +0000

    added fzf as dependency for telescope for faster sorting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,11 @@ return	{
     {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'nvim-telescope/telescope-fzf-native.nvim'
+        },
 	config = function()
 
 		local builtin = require('telescope.builtin')
